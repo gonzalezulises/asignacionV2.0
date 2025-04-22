352ca54c2572edc1aa93da22a739c4e5dca12faf
Ulises González <gonzalez.ulises@gmail.com>
2025-04-22 00:49:07 -0500

    Configuración inicial del proyecto con estructura requerida por el profesor

---
+++ src/main/scala/examen/examen.scala
@@ -0,0 +1,93 @@
+package examen
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
+object examen {
+
+  /**
+   * Ejercicio 1: Crear un DataFrame y realizar operaciones básicas
+   * 
+   * Pregunta: Crea un DataFrame a partir de una secuencia de tuplas que contenga información sobre
+   *           estudiantes (nombre, edad, calificación).
+   *         Realiza las siguientes operaciones:
+   *          Muestra el esquema del DataFrame.
+   *          Filtra los estudiantes con una calificación mayor a 8.
+   *          Selecciona los nombres de los estudiantes y ordénalos por calificación de forma descendente.
+   */
+  def ejercicio1(estudiantes: DataFrame): DataFrame = {
+    estudiantes.printSchema()
+    estudiantes
+      .select("nombre")
+      .orderBy(desc("calificacion"))
+  }
+
+  /**
+   * Ejercicio 2: UDF (User Defined Function)
+   * 
+   * Pregunta: Define una función que determine si un número es par o impar.
+   *           Aplica esta función a una columna de un DataFrame que contenga una lista de números.
+   */
+  def ejercicio2(numeros: DataFrame): DataFrame = {
+    val parImparUDF = udf((numero: Int) => {
+      if (numero % 2 == 0) "Par" else "Impar"
+    })
+    
+    numeros.select(parImparUDF(col("numero")).as("paridad"))
+  }
+
+  /**
+   * Ejercicio 3: Joins y agregaciones
+   * 
+   * Pregunta: Dado dos DataFrames,
+   *           uno con información de estudiantes (id, nombre)
+   *           y otro con calificaciones (id_estudiante, asignatura, calificacion),
+   *           realiza un join entre ellos y calcula el promedio de calificaciones por estudiante.
+   */
+  def ejercicio3(estudiantes: DataFrame, calificaciones: DataFrame): DataFrame = {
+    val joinedDF = estudiantes.join(
+      calificaciones,
+      estudiantes("id") === calificaciones("id_estudiante"),
+      "inner"
+    )
+    
+    joinedDF
+      .groupBy("id", "nombre")
+      .agg(
+        avg("calificacion").alias("promedio_calificaciones")
+      )
+      .orderBy("id")
+  }
+
+  /**
+   * Ejercicio 4: Uso de RDDs
+   * 
+   * Pregunta: Crea un RDD a partir de una lista de palabras y 
+   *           cuenta la cantidad de ocurrencias de cada palabra.
+   */
+  def ejercicio4(palabras: List[String])(implicit spark: SparkSession): RDD[(String, Int)] = {
+    val palabrasRDD = spark.sparkContext.parallelize(palabras)
+    
+    palabrasRDD
+      .map(palabra => (palabra, 1))
+      .reduceByKey(_ + _)
+      .sortBy(_._2, ascending = false)
+  }
+
+  /**
+   * Ejercicio 5: Procesamiento de archivos
+   * 
+   * Pregunta: Carga un archivo CSV que contenga información sobre
+   *           ventas (id_venta, id_producto, cantidad, precio_unitario)
+   *           y calcula el ingreso total (cantidad * precio_unitario) por producto.
+   */
+  def ejercicio5(ventas: DataFrame): DataFrame = {
+    ventas
+      .withColumn("ingreso", col("cantidad").cast("int") * col("precio_unitario").cast("double"))
+      .groupBy("id_producto")
+      .agg(
+        sum("ingreso").alias("ingreso_total")
+      )
+  }
+}
---
+++ src/test/scala/examen/examenTest.scala
@@ -0,0 +1,77 @@
+package examen
+
+import utils.TestInit
+import examen._
+
+class examenTest extends TestInit {
+
+  implicit val sc = spark.sparkContext
+
+  "Ejercicio 1" should "Selecciona los nombres de los estudiantes y ordénalos por calificación de forma descendente" in {
+    import spark.implicits._
+
+    val estudiantes = Seq(
+      ("Ana", 23, 9.0),
+      ("Luis", 21, 7.5),
+      ("Pedro", 22, 8.5),
+      ("Maria", 20, 9.5)
+    ).toDF("nombre", "edad", "calificacion")
+
+    val out = ejercicio1(estudiantes).collect().map(_.getString(0))
+
+    out shouldBe List("Maria", "Ana", "Pedro", "Luis")
+  }
+
+  "Ejercicio 2" should "Devuelve los datos paritarios" in {
+    import spark.implicits._
+
+    val numeros = Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).toDF("numero")
+
+    val out = ejercicio2(numeros).collect().map(_.getString(0))
+
+    out shouldBe List("Impar", "Par", "Impar", "Par", "Impar", "Par", "Impar", "Par", "Impar", "Par")
+  }
+
+
+  "Ejercicio 3" should "calcula el promedio de calificaciones por estudiante" in {
+    import spark.implicits._
+
+    val estudiantes = Seq(
+      (1, "Ana"),
+      (2, "Luis"),
+      (3, "Pedro"),
+      (4, "Maria")
+    ).toDF("id", "nombre")
+
+    val calificaciones = Seq(
+      (1, "Matematicas", 9.0),
+      (1, "Historia", 8.5),
+      (2, "Matematicas", 7.5),
+      (3, "Historia", 8.0),
+      (4, "Matematicas", 9.5),
+      (4, "Historia", 9.0)
+    ).toDF("id_estudiante", "asignatura", "calificacion")
+
+    val out = ejercicio3(estudiantes, calificaciones).collect().map(x => (x.get(0), x.get(1), x.get(2)))
+
+    out shouldBe List((1, "Ana", 8.75), (2, "Luis", 7.5), (3, "Pedro", 8.0), (4, "Maria", 9.25))
+  }
+
+  "Ejercicio 4" should "cuenta la cantidad de ocurrencias de cada palabra" in {
+    val palabras = List("spark", "hadoop", "spark", "hive", "spark", "hadoop", "hive")
+
+    val out = ejercicio4(palabras).collect().map(x => (x._1, x._2)).sorted
+
+    out shouldBe Array(("hive", 2), ("spark", 3), ("hadoop", 2)).sorted
+  }
+
+  "Ejercicio 5" should "calcula el ingreso total" in {
+    val ventas = spark.read.option("header", "true").csv("src/test/resources/examen/ventas.csv")
+
+    val out = ejercicio5(ventas).collect().map(x => (x.getString(0).toInt, x.get(1)))
+
+    out.toList shouldBe List((101, 460.0), (107, 396.0), (110, 494.0),
+                       (104, 800.0), (102, 405.0), (103, 280.0),
+                       (108, 486.0), (106, 425.0), (105, 570.0), (109, 540.0))
+  }
+}
